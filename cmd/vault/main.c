8d55f84e4c1d84c8d15289b0d8d88c7fb0546b15
Jianwei Xie <xiejw0217+db@gmail.com>
2022-04-07 15:49:49 -0700

    print the whole tree.

--- cmd/vault/main.c
+++ cmd/vault/main.c
@@ -294,21 +294,37 @@ ftFree(struct ft_node *root)
         ftSubTreeFree(root);
 }
 
+static void
+ftDumpImpl(int fd, struct ft_node *node, sds_t space)
+{
+        int is_dir;
+        // append 4 spaces.
+        sdsCatPrintf(&space, "    ");
+
+        for (size_t i = 0; i < vecSize(node->children); i++) {
+                struct ft_node *child = node->children[i];
+                is_dir                = child->is_dir;
+                dprintf(fd, "%s+-> %s%s\n", space, child->path,
+                        is_dir ? " (+)" : "");
+
+                if (is_dir) {
+                        ftDumpImpl(fd, child, space);
+                }
+        }
+
+        // remove 4 spaces
+        size_t len = sdsLen(space);
+        sdsSetLen(space, len - 4);
+        space[len - 4] = 0;
+}
+
 // dump the tree representation to fd (say stdout).
 void
 ftDump(int fd, struct ft_node *root)
 {
         sds_t space = sdsEmpty();
+        dprintf(fd, "%sroot - %s\n", space, root->root_dir);
 
-        if (root->parent == NULL) dprintf(fd, "%snode (root)\n", space);
-
-        sdsCatPrintf(&space, "    ");
-
-        for (size_t i = 0; i < vecSize(root->children); i++) {
-                struct ft_node *child = root->children[i];
-                dprintf(fd, "%s+-> %s %s\n", space, child->path,
-                        child->is_dir ? "(+)" : "");
-        }
-
+        ftDumpImpl(fd, root, space);
         sdsFree(space);
 }
