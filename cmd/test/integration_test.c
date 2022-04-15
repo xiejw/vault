7df49a5f74aa03a8dcebd2bc0dcb493e1041940e
Jianwei Xie <xiejw0217+db@gmail.com>
2022-04-15 16:48:30 -0700

    added children check.

--- cmd/test/integration_test.c
+++ cmd/test/integration_test.c
@@ -11,6 +11,15 @@ static char *
 test_walk_tree()
 {
         error_t err;
+        // expected data:
+        //     tests/test_tree/a
+        //     tests/test_tree/a/h
+        //     tests/test_tree/a/d
+        //     tests/test_tree/a/e
+        //     tests/test_tree/a/e/f
+        //     tests/test_tree/a/e/f/g
+        //     tests/test_tree/a/b
+        //     tests/test_tree/a/b/c
         struct ft_node *root = ftRootNew(sdsNew("tests/test_tree/a"));
 
         struct ft_walk_config cfg = {
@@ -24,6 +33,10 @@ test_walk_tree()
                     strcmp(root->root_dir, "tests/test_tree/a") == 0);
         ASSERT_TRUE("root path", strcmp(root->path, "") == 0);
         ASSERT_TRUE("root is_dir", root->is_dir == 1);
+        ASSERT_TRUE("root is_dir", root->is_dir == 1);
+        ASSERT_TRUE("root children count", vecSize(root->children) == 4);
+
+        // TODO check all chidren once sorting is done
 
         ftFree(root);
         return NULL;
