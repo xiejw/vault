5b1c7e057090f3363b8153012d7f6c81ee345ae5
Jianwei Xie <xiejw0217+db@gmail.com>
2022-04-15 11:57:31 -0700

    added real tests for integration.

--- cmd/test/integration_test.c
+++ cmd/test/integration_test.c
@@ -1,8 +1,26 @@
 #include "testing/testing.h"
 
+// eva
+#include <adt/sds.h>
+#include <base/types.h>
+
+// vault
+#include "ft_walk.h"
+
 static char *
 test_walk_tree()
 {
+        error_t err;
+        struct ft_node *root = ftRootNew(sdsNew("tests/test_tree/a"));
+
+        struct ft_walk_config cfg = {
+            .dangling_sym_link = FT_SILENT,  // for testing, we ignore it.
+        };
+
+        err = ftWalk(root, &cfg);
+        ASSERT_TRUE("no err", err == OK);
+
+        ftFree(root);
         return NULL;
 }
 
