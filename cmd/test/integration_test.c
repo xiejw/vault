7e4421e6f35643dabe893b7a06195c214a964e30
xiejw0217+xieqi-arch <xiejw0217+xieqi-arch@gmail.com>
2022-04-24 13:24:04 -0700

    tested all children are visited.

--- cmd/test/integration_test.c
+++ cmd/test/integration_test.c
@@ -26,17 +26,41 @@ test_walk_tree()
             .dangling_sym_link = FT_SILENT,  // for testing, we ignore it.
         };
 
+        // walk and expect no issue.
         err = ftWalk(root, &cfg);
         ASSERT_TRUE("no err", err == OK);
 
+        // examine root node.
         ASSERT_TRUE("root dir",
                     strcmp(root->root_dir, "tests/test_tree/a") == 0);
         ASSERT_TRUE("root path", strcmp(root->path, "") == 0);
         ASSERT_TRUE("root is_dir", root->is_dir == 1);
         ASSERT_TRUE("root is_dir", root->is_dir == 1);
+
+        // examine children
         ASSERT_TRUE("root children count", vecSize(root->children) == 4);
 
-        // TODO check all chidren once sorting is done
+        int mask = 0;  // records the bits so we know all children are visited.
+        struct ft_node *child;
+        for (size_t i = 0; i < vecSize(root->children); i++) {
+                child = root->children[i];
+                if (strcmp(child->path, "h") == 0) {
+                        mask |= 1;
+                        continue;
+                } else if (strcmp(child->path, "d") == 0) {
+                        mask |= 2;
+                        continue;
+                } else if (strcmp(child->path, "e") == 0) {
+                        mask |= 4;
+                        continue;
+                } else if (strcmp(child->path, "b") == 0) {
+                        mask |= 8;
+                        continue;
+                }
+                ASSERT_TRUE("should not reach", 0);
+        }
+
+        ASSERT_TRUE("found all children", mask == 15);
 
         ftFree(root);
         return NULL;
